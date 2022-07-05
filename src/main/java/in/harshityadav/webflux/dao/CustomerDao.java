385d9eeb3ae7dbfcbd564e0fd6da0affc1218f0f
Harshit Yadav <harshityadav@outlook.com>
2022-07-06 00:52:45 +0530

    Sleep Response

--- src/main/java/in/harshityadav/webflux/dao/CustomerDao.java
+++ src/main/java/in/harshityadav/webflux/dao/CustomerDao.java
@@ -10,9 +10,20 @@ import java.util.stream.IntStream;
 @Component
 public class CustomerDao {
 
+
+
+    private static void sleepExecution(int j){
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
     public List<Customer> getCustomers(){
 
         return IntStream.rangeClosed(1,50)
+                .peek(CustomerDao::sleepExecution)
                 .peek(i-> System.out.println("Processing Count"+i))
                 .mapToObj(i-> new Customer(i,"Cusomer"+i))
                 .collect(Collectors.toList());
